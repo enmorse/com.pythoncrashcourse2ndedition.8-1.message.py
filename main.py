287533eaa679a66e90ea3ddfa16b2b820cce1b1c
Ernest Morse <enmorse48@gmail.com>
2021-12-10 13:48:18 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# Write a function named display_message that
+# prints one sentence telling everyone what you
+# are learning about in this chapter. Call the
+# function, and make sure the message displays
+# correctly.
+def display_message():
+    print("In this chapter, I learned functions.")
+
+
+display_message()
